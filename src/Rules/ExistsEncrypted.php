8b69c6f874416934bc3c805c3a203540f39b96df
riccardodallavia <riccardo.dallavia@h-farm.com>
2021-10-05 17:24:00 +0200

    FIX custom rules validation

--- src/Rules/ExistsEncrypted.php
+++ src/Rules/ExistsEncrypted.php
@@ -5,12 +5,15 @@ namespace HFarm\Encryptable\Rules;
 use HFarm\Encryptable\Encryption;
 use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 use Illuminate\Validation\Rules\Exists;
 
 class ExistsEncrypted extends Exists implements Rule
 {
     public function passes($attribute, $value): bool
     {
+        $attribute = Str::before($attribute, '.');
+
         return ! Validator::make([
             $attribute => Encryption::php()->encrypt($value),
         ], [
