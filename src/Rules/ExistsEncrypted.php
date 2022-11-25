1a4c15173c1e48368d854a7ca4d7f0c012ec60a4
riccardodallavia <riccardo.dallavia@h-farm.com>
2022-11-25 15:25:17 +0100

    FIX custom rules minor issues

--- src/Rules/ExistsEncrypted.php
+++ src/Rules/ExistsEncrypted.php
@@ -5,11 +5,28 @@ namespace Maize\Encryptable\Rules;
 use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
+use Illuminate\Support\Traits\ForwardsCalls;
 use Illuminate\Validation\Rules\Exists;
 use Maize\Encryptable\Encryption;
 
-class ExistsEncrypted extends Exists implements Rule
+class ExistsEncrypted implements Rule
 {
+    use ForwardsCalls;
+
+    private Exists $rule;
+
+    public function __construct(string $table, string $column = 'NULL')
+    {
+        $this->rule = new Exists($table, $column);
+    }
+
+    public function __call(string $name, array $arguments)
+    {
+        $this->forwardCallTo($this->rule, $name, $arguments);
+
+        return $this;
+    }
+
     public function passes($attribute, $value): bool
     {
         $attribute = Str::before($attribute, '.');
@@ -17,7 +34,7 @@ class ExistsEncrypted extends Exists implements Rule
         return ! Validator::make([
             $attribute => Encryption::php()->encrypt($value),
         ], [
-            $attribute => (string) $this,
+            $attribute => $this->rule,
         ])->fails();
     }
 
--- src/Rules/UniqueEncrypted.php
+++ src/Rules/UniqueEncrypted.php
@@ -5,11 +5,28 @@ namespace Maize\Encryptable\Rules;
 use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
+use Illuminate\Support\Traits\ForwardsCalls;
 use Illuminate\Validation\Rules\Unique;
 use Maize\Encryptable\Encryption;
 
-class UniqueEncrypted extends Unique implements Rule
+class UniqueEncrypted implements Rule
 {
+    use ForwardsCalls;
+
+    private Unique $rule;
+
+    public function __construct(string $table, string $column = 'NULL')
+    {
+        $this->rule = new Unique($table, $column);
+    }
+
+    public function __call(string $name, array $arguments)
+    {
+        $this->forwardCallTo($this->rule, $name, $arguments);
+
+        return $this;
+    }
+
     public function passes($attribute, $value): bool
     {
         $attribute = Str::before($attribute, '.');
@@ -17,7 +34,7 @@ class UniqueEncrypted extends Unique implements Rule
         return ! Validator::make([
             $attribute => Encryption::php()->encrypt($value),
         ], [
-            $attribute => (string) $this,
+            $attribute => $this->rule,
         ])->fails();
     }
 
